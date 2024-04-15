9d14b5b1cf9d99731ebb4bad12be2ee7f20580df
Siju Babu <sanu98@gmail.com>
2024-04-15 23:53:17 +0530

    updated

--- CoreJava_FunctionalProgramming/src/core/java/functional/programming/FP02BehaviourParameterization.java
+++ CoreJava_FunctionalProgramming/src/core/java/functional/programming/FP02BehaviourParameterization.java
@@ -3,9 +3,12 @@ package core.java.functional.programming;
 import java.util.List;
 import java.util.function.Predicate;
 
-//Here we are just trying out can we pass the behavior to another function.
+//Here we are just trying out can we pass the behavior/function to another function.
 //Predicate<Integer> integerPredicate = x-> x%2==0;
-//Here can we pass the behavior 'integerPredicate' to another function.
+//Here can we pass the behavior/function 'integerPredicate' to another function.
+//Below two points are important things to remember.
+//We assign a function to a variable
+//We passes a function to another function
 public class FP02BehaviourParameterization {
 
 	public static void main(String ar[]) {
@@ -13,7 +16,7 @@ public class FP02BehaviourParameterization {
 		behaviorParameterization(integers);
 	}
 	
-	//This is the example of behavior parameterization
+	//This is the example of behavior /function parameterization
 	//This will help you in most of the case where you pass the algorithm and rest of the things will be n a single place
 	public static void behaviorParameterization(List<Integer> integers) {
 		Predicate<Integer> evenPredicate = x->x%2==0;
@@ -23,7 +26,8 @@ public class FP02BehaviourParameterization {
 		
 	}
 	
-	//Common place where we pass the behavior as parameter
+	//Common place where we pass the behavior/function as parameter
+	//We passing a function to another function.
 	public static void filterAndPrint(List<Integer> integers, Predicate<Integer> predicate) {
 		integers.stream().filter(predicate).forEach(System.out::println);
 	}
